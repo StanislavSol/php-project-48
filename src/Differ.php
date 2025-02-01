53df0be75053f7a6cb4fe030da93d774c6c2f4c2
имя фамилия < >
2025-02-01 13:09:34 +0300

    add diff

--- src/Differ.php
+++ src/Differ.php
@@ -1,3 +1,47 @@
 <?php
 
 namespace Differ\Differ;
+
+function getDiffData($fileOne, $fileTwo)
+{
+    $dataMergeKeys = array_keys(array_merge($fileOne, $fileTwo));
+
+    $resultDiff = [];
+
+    foreach ($dataMergeKeys as $key) {
+        if (!array_key_exists($key, $fileOne)) {
+            $resultDiff[] = [
+                'name' => $key,
+                'diff' => 'added',
+                'value' => $fileTwo[$key]
+            ];
+        } elseif (!array_key_exists($key, $fileTwo)) {
+            $resultDiff[] = [
+                'name' => $key,
+                'diff' => 'deleted',
+                'value' => $fileOne[$key]
+            ];
+        } elseif (is_array($fileOne[$key]) && is_array($fileTwo[$key]) && $fileOne[$key] === $fileTwo[$key]) {
+           $resultDiff[] = [
+                'name' => $key,
+                'diff' => 'nested',
+                'children' => getDiffData($fileOne[$key], $fileTwo[$key])
+            ]; 
+        } elseif ($fileOne[$key] === $fileTwo[$key]) {
+            $resultDiff[] = [
+                'name' => $key,
+                'diff' => 'unchenged',
+                'value' => $fileOne[$key]
+            ];
+        } elseif ($fileOne[$key] !== $fileTwo[$key]) {
+            $resultDiff[] = [
+                'name' => $key,
+                'diff' => 'chenged',
+                'valueOne' => $fileOne[$key],
+                'valueTwo' => $fileTwo[$key]
+            ];
+        }
+
+        return $resultDiff;
+    }
+}
